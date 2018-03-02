549fbc94b4ed5a190ec8a8918836c1aaf4cfe595
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-01 21:03:50 -1000

    add assets and helper function to handle GET requests

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,13 @@
 module.exports = `
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>The Elements</title>
   <link rel="stylesheet" href="/css/styles.css">
 </head>
+
 <body>
   <h1>The Elements</h1>
   <h2>These are all the known elements.</h2>
@@ -19,4 +21,5 @@ module.exports = `
     </li>
   </ol>
 </body>
+
 </html>`;
--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 const net = require("net");
+const files = {
+  ["/"]: require("./assets/index.js"),
+  ["/index.html"]: require("./assets/index.js"),
+  ["/css/styles.css"]: require("./assets/styles.js"),
+  ["/hydrogen.html"]: require("./assets/hydrogen.js"),
+  ["/helium.html"]: require("./assets/helium.js"),
+  ["/404.html"]: require("./assets/404.js")
+};
+
 const PORT = 8080;
 
 const server = net.createServer(client => {
   console.log("client connected");
-
+  client.setEncoding("utf8");
   client.on("data", data => {
-    const dataStr = data.toString();
-    console.log(dataStr.split("\n"));
-    client.write(`HTTP/1.1 200 OK 
-    \n
-    \n
-    <h1>Hello World!</h1>`);
+    request(data, client);
   });
 });
+
+const request = (data, client) => {
+  const reqLine = data.split("\n")[0].split(" ");
+  const reqMethod = reqLine[0];
+  const reqURI = reqLine[1];
+  const reqHTTPVersion = reqLine[2];
+  const resHTTPVersion = "HTTP/1.1 200 OK";
+
+  const checkURI = key => {
+    if (files[key]) {
+      return files[key];
+    } else {
+      return files["/404.html"];
+    }
+  };
+
+  if (reqMethod === "GET") {
+    const body = checkURI(reqURI);
+    const message = `${resHTTPVersion}\n\n${body}`;
+    client.write(message);
+    client.end();
+  } else {
+    const body = files["/404.html"];
+    const message = `${resHTTPVersion}\n\n${body}`;
+    client.write(message);
+    client.end();
+  }
+};
 
 server.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
